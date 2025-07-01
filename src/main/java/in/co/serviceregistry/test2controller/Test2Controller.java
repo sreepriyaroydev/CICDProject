c7fbad9298cd4cdc4bb8c98838bcc102d80da815
sreepriyaroy <sreepriya.roy@greenwave.co.in>
2025-07-01 15:23:06 +0530

    commit

---
+++ src/main/java/in/co/serviceregistry/test2controller/Test2Controller.java
@@ -0,0 +1,14 @@
+package in.co.serviceregistry.test2controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test2")
+public class Test2Controller {
+	@GetMapping("/check")
+	public String testcheck() {
+		return "Test2 check successful";
+	}
+}
